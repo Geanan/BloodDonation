ef71eac90e2bb4b39d8d1f73671dd0ad95686892
cristianatolea <cristiana.tolea@roweb.ro>
2018-12-06 20:44:07 +0200

    modified adauga utilizator

--- adaugaUtilizator.php
+++ adaugaUtilizator.php
@@ -7,13 +7,13 @@ if (!$con) {
  
 mysqli_select_db($con, "blood_donation"); 
 
-$username = trim($_POST['username']);
 $nume = $_POST['nume'];
+$username = trim($_POST['username']);
 $parola = trim($_POST['parola']);
 $profil = $_POST['profil'];
 $grupaSangvina = $_POST['grupaSangvina']
 
-mysqli_query($con, "insert into utilizator (username, nume, parola, profil, grupa sangvina) values ( '$username','$nume','$parola','$profil','$grupaSangvina')");
+mysqli_query($con, "insert into utilizator ( nume, username, parola, profil, grupa sangvina) values ( '$nume', '$username','$parola','$profil','$grupaSangvina')");
 mysqli_close($con); 
  
 header("Location: adauga.php");  
