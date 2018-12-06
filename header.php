992169a5f3eba5e564d91d3a66e4ff0198fe72e1
cristianatolea <cristiana.tolea@roweb.ro>
2018-12-06 20:08:54 +0200

    modified header

--- header.php
+++ header.php
@@ -2,15 +2,14 @@
  session_start(); 
 ?>
 
-<div class="wrapper", style="background-color:lightblue">
+<div class="wrapper">
 	<nav>
 		<ul>
 			<li><a href="index.php" >Acasă</a></li>
 			<li><a href="evenimente.php">Evenimente</a></li>
 			
-			<?php if(isset($_SESSION['nume']) && $_SESSION['profil'] == 'admin' )  {?>
 			<li><a href="adauga.php">Adaugă</a></li>
-			<?php } ?>	
+	
 			
 			<?php if(isset($_SESSION['nume']) && $_SESSION['profil'] == 'cinefil' )  {?>
 			<li><a href="myCinema.php">Cinemaul meu</a></li>
